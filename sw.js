6d9dd660d92b9ea36e4cda8f06b7e4a1fd4a0a24
Tomasz Bochenek <bochenekt@gmail.com>
2018-08-09 22:20:23 +0200

    fix: add subpages to cached files in service worker

--- sw.js
+++ sw.js
@@ -19,6 +19,16 @@ const cachedFiles = [
 	"./js/restaurant_info.js",
 	"./index.html",
 	"./restaurant.html",
+	"./restaurant.html?id=1",
+	"./restaurant.html?id=2",
+	"./restaurant.html?id=3",
+	"./restaurant.html?id=4",
+	"./restaurant.html?id=5",
+	"./restaurant.html?id=6",
+	"./restaurant.html?id=7",
+	"./restaurant.html?id=8",
+	"./restaurant.html?id=9",
+	"./restaurant.html?id=10",
 ] ;
 
 self.addEventListener("install", function(event) {
